5aa443604dd55d735d659a69e26833d674a53d99
Fernando López <ferlopezm94@gmail.com>
2020-03-30 01:43:58 -0600

    style(index): add better styles for xs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,42 +9,52 @@ const IndexPage = () => (
       description='Nacional: 993 casos confirmados (145 más que ayer). 20 defunciones (4 más que ayer).'
     />
     <div className='h-9/10 w-11/12'>
-      <h2 className='text-4xl text-center leading-10 font-extrabold text-blue-600 sm:text-5xl sm:leading-none md:text-5xl mb-4'>
+      <h1 className='text-2xl sm:text-4xl md:text-5xl text-center leading-6 sm:leading-none font-extrabold text-blue-600 mb-4'>
         COVID-19
         <br className='xl:hidden' />
         <span className='text-gray-900'> en México</span>
-      </h2>
-      <p className='text-center text-gray-600 mb-8 text-lg sm:text-2xl'>
+      </h1>
+      <p className='text-center text-gray-600 mb-8 text-base sm:text-2xl'>
         Estadísticas al día 29 de marzo, 2020
       </p>
       <div className='h-7/10'>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2 rounded-t-lg'>
-          <p className='text-3xl font-extrabold'>993</p>
-          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>
+          <p className='text-xl sm:text-3xl font-extrabold'>993</p>
+          <p className='uppercase tracking-wide text-xs sm:text-sm text-gray-600 font-bold'>
             Casos confirmados
           </p>
-          <span className='text-sm font-light text-gray-600 italic'>145 más que ayer</span>
+          <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
+            145 más que ayer
+          </span>
         </div>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2'>
-          <p className='text-3xl font-extrabold'>20</p>
-          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>Defunciones</p>
-          <span className='text-sm font-light text-gray-600 italic'>4 más que ayer</span>
+          <p className='text-xl sm:text-3xl font-extrabold'>20</p>
+          <p className='uppercase tracking-wide text-xs sm:text-sm text-gray-600 font-bold'>
+            Defunciones
+          </p>
+          <span className='text-xs sm:text-sm font-light text-gray-600 italic'>4 más que ayer</span>
         </div>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2'>
-          <p className='text-3xl font-extrabold'>2,564</p>
-          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>
+          <p className='text-xl sm:text-3xl font-extrabold'>2,564</p>
+          <p className='uppercase tracking-wide text-xs sm:text-sm text-gray-600 font-bold'>
             Casos sospechosos
           </p>
-          <span className='text-sm font-light text-gray-600 italic'>59 menos que ayer</span>
+          <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
+            59 menos que ayer
+          </span>
         </div>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center rounded-b-lg'>
-          <p className='text-3xl font-extrabold'>4,955</p>
-          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>Casos negativos</p>
-          <span className='text-sm font-light text-gray-600 italic'>614 más que ayer</span>
+          <p className='text-xl sm:text-3xl font-extrabold'>4,955</p>
+          <p className='uppercase tracking-wide text-xs sm:text-sm text-gray-600 font-bold'>
+            Casos negativos
+          </p>
+          <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
+            614 más que ayer
+          </span>
         </div>
       </div>
     </div>
-    <span className='text-sm font-light text-gray-600'>
+    <span className='text-xs sm:text-sm font-light text-gray-600'>
       Fuente:{' '}
       <a
         href='https://www.gob.mx/salud/documentos/coronavirus-covid-19-comunicado-tecnico-diario-238449'
