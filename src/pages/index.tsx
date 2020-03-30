8c356d531a04d06913295915e85f8fa03e1f5d8f
Fernando López <ferlopezm94@gmail.com>
2020-03-30 00:49:45 -0600

    feat(index): rearrange information and update seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,47 +3,58 @@ import React from 'react';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <div className='w-screen h-screen bg-gray-200 flex justify-center items-center'>
+  <div className='w-screen h-screen bg-gray-200 flex flex-col justify-center items-center'>
     <SEO
-      title='COVID-19 MX | Nacional'
-      description='29 de marzo, 2020: 993 casos confirmados (+145). 20 defunciones (+4). 2,564 casos sospechos (-59). 4,955 casos negativos (+614).'
+      title='29 de marzo, 2020'
+      description='Nacional: 993 casos confirmados (145 más que ayer). 20 defunciones (4 más que ayer).'
     />
     <div className='h-9/10 w-11/12'>
-      <h2 className='text-4xl text-center leading-10 font-extrabold text-blue-600 sm:text-5xl sm:leading-none md:text-6xl mb-4'>
+      <h2 className='text-4xl text-center leading-10 font-extrabold text-blue-600 sm:text-5xl sm:leading-none md:text-5xl mb-4'>
         COVID-19
         <br className='xl:hidden' />
         <span className='text-gray-900'> en México</span>
       </h2>
-      <p className='text-center text-gray-600 mb-8 text-lg sm:text-3xl'>
+      <p className='text-center text-gray-600 mb-8 text-lg sm:text-2xl'>
         Estadísticas al día 29 de marzo, 2020
       </p>
-      <div className='container h-7/10'>
+      <div className='h-7/10'>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2 rounded-t-lg'>
-          <p className='text-3xl font-extrabold'>
-            993 <span className='text-base font-light text-gray-600'>+145</span>
+          <p className='text-3xl font-extrabold'>993</p>
+          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>
+            Casos confirmados
           </p>
-          <p className='uppercase tracking-wide text-sm text-gray-600'>Casos confirmados</p>
+          <span className='text-sm font-light text-gray-600 italic'>145 más que ayer</span>
         </div>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2'>
-          <p className='text-3xl font-extrabold'>
-            20 <span className='text-base font-light text-gray-600'>+4</span>
-          </p>
-          <p className='uppercase tracking-wide text-sm text-gray-600'>Defunciones</p>
+          <p className='text-3xl font-extrabold'>20</p>
+          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>Defunciones</p>
+          <span className='text-sm font-light text-gray-600 italic'>4 más que ayer</span>
         </div>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2'>
-          <p className='text-3xl font-extrabold'>
-            2,564 <span className='text-base font-light text-gray-600'>-59</span>
+          <p className='text-3xl font-extrabold'>2,564</p>
+          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>
+            Casos sospechosos
           </p>
-          <p className='uppercase tracking-wide text-sm text-gray-600'>Casos sospechosos</p>
+          <span className='text-sm font-light text-gray-600 italic'>59 menos que ayer</span>
         </div>
         <div className='bg-white h-1/4 flex flex-col justify-center items-center rounded-b-lg'>
-          <p className='text-3xl font-extrabold'>
-            4,955 <span className='text-base font-light text-gray-600'>+614</span>
-          </p>
-          <p className='uppercase tracking-wide text-sm text-gray-600'>Casos negativos</p>
+          <p className='text-3xl font-extrabold'>4,955</p>
+          <p className='uppercase tracking-wide text-sm text-gray-600 font-bold'>Casos negativos</p>
+          <span className='text-sm font-light text-gray-600 italic'>614 más que ayer</span>
         </div>
       </div>
     </div>
+    <span className='text-sm font-light text-gray-600'>
+      Fuente:{' '}
+      <a
+        href='https://www.gob.mx/salud/documentos/coronavirus-covid-19-comunicado-tecnico-diario-238449'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='underline'
+      >
+        Gobierno de México
+      </a>
+    </span>
   </div>
 );
 
