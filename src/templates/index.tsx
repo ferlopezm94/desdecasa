9499c40ca2dd712abcaec1f7ba78f13d7b0854ce
Fernando López <ferlopezm94@gmail.com>
2020-04-01 02:09:06 -0600

    refactor(states): extract text difference creation

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -35,26 +35,37 @@ interface Props {
 const IndexPage = ({ path, pageContext }: Props) => {
   const { stateName, date, today, yesterday } = pageContext;
   console.log('start', path, stateName, today, yesterday);
+
   const todayDate = moment(date).format('DD [de] MMMM[,] YYYY');
   const differenceConfirmed = today.confirmed - yesterday.confirmed;
   const differenceDeaths = today.deaths - yesterday.deaths;
   const differenceSuspects = today.suspects - yesterday.suspects;
   const differenceNegatives = today.negatives - yesterday.negatives;
+  const differenceTests = today.tests && yesterday.tests ? today.tests - yesterday.tests : 0;
 
-  const sharingMessage = `*${todayDate} | ${pageContext.stateName}:*%0A
-- ${today.confirmed} casos confirmados (${Math.abs(differenceConfirmed)} ${
+  const differenceConfirmedText = `${Math.abs(differenceConfirmed)} ${
     differenceConfirmed >= 0 ? 'más' : 'menos'
-  } que ayer)%0A
-- ${today.deaths} defunciones (${Math.abs(differenceDeaths)} ${
+  } que ayer`;
+  const differenceDeathsText = `${Math.abs(differenceDeaths)} ${
     differenceDeaths >= 0 ? 'más' : 'menos'
-  } que ayer)%0A
-- ${today.suspects} casos sospechosos (${Math.abs(differenceSuspects)} ${
+  } que ayer`;
+  const differenceSuspectsText = `${Math.abs(differenceSuspects)} ${
     differenceSuspects >= 0 ? 'más' : 'menos'
-  } que ayer)%0A
-- ${today.negatives} casos negativos (${Math.abs(differenceNegatives)} ${
+  } que ayer`;
+  const differenceNegativesText = `${Math.abs(differenceNegatives)} ${
     differenceNegatives >= 0 ? 'más' : 'menos'
-  } que ayer)%0A
-${today.tests && `- ${today.tests} personas estudiadas%0A`}
+  } que ayer`;
+  const differenceTestsText =
+    today.tests && yesterday.tests
+      ? `${Math.abs(differenceTests)} ${differenceTests >= 0 ? 'más' : 'menos'} que ayer`
+      : '';
+
+  const sharingMessage = `*${todayDate} | ${stateName}:*%0A
+- ${today.confirmed} casos confirmados (${differenceConfirmedText})%0A
+- ${today.deaths} defunciones (${differenceDeathsText})%0A
+- ${today.suspects} casos sospechosos (${differenceSuspectsText})%0A
+- ${today.negatives} casos negativos (${differenceNegativesText})%0A
+${today.tests ? `- ${today.tests} personas estudiadas (${differenceTestsText})%0A` : ''}
 
 %0A%23QuedateEnCasa 🏠
 %0AInformación diaria y detallada en https://desdecasa.today/`;
@@ -63,13 +74,7 @@ ${today.tests && `- ${today.tests} personas estudiadas%0A`}
     <div className='w-screen h-screen bg-gray-200 flex flex-col justify-center items-center'>
       <SEO
         title={todayDate}
-        description={`${pageContext.stateName}: ${today.confirmed} casos confirmados (${Math.abs(
-          differenceConfirmed,
-        )} ${differenceConfirmed >= 0 ? 'más' : 'menos'} que ayer). ${
-          today.deaths
-        } defunciones (${Math.abs(differenceDeaths)} ${
-          differenceDeaths >= 0 ? 'más' : 'menos'
-        } que ayer).`}
+        description={`${pageContext.stateName}: ${today.confirmed} casos confirmados (${differenceConfirmedText}). ${today.deaths} defunciones (${differenceDeathsText}).`}
       />
       <div className='h-9/10 w-4/5 sm:w-3/5 lg:w-2/5'>
         <h1 className='text-2xl sm:text-4xl md:text-5xl text-center leading-6 sm:leading-none font-extrabold text-blue-600 mb-3'>
@@ -85,7 +90,7 @@ ${today.tests && `- ${today.tests} personas estudiadas%0A`}
               Casos confirmados
             </p>
             <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
-              {Math.abs(differenceConfirmed)} {differenceConfirmed >= 0 ? 'más' : 'menos'} que ayer
+              {differenceConfirmedText}
             </span>
           </div>
           <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2'>
@@ -94,7 +99,7 @@ ${today.tests && `- ${today.tests} personas estudiadas%0A`}
               Defunciones
             </p>
             <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
-              {Math.abs(differenceDeaths)} {differenceDeaths >= 0 ? 'más' : 'menos'} que ayer
+              {differenceDeathsText}
             </span>
           </div>
           <div className='bg-white h-1/4 flex flex-col justify-center items-center border-b-2'>
@@ -103,7 +108,7 @@ ${today.tests && `- ${today.tests} personas estudiadas%0A`}
               Casos sospechosos
             </p>
             <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
-              {Math.abs(differenceSuspects)} {differenceSuspects >= 0 ? 'más' : 'menos'} que ayer
+              {differenceSuspectsText}
             </span>
           </div>
           <div className='bg-white h-1/4 flex flex-col justify-center items-center rounded-b-lg'>
@@ -112,13 +117,15 @@ ${today.tests && `- ${today.tests} personas estudiadas%0A`}
               Casos negativos
             </p>
             <span className='text-xs sm:text-sm font-light text-gray-600 italic'>
-              {Math.abs(differenceNegatives)} {differenceNegatives >= 0 ? 'más' : 'menos'} que ayer
+              {differenceNegativesText}
             </span>
           </div>
         </div>
 
         <div className='flex flex-col mb-4'>
-          <p className='text-sm text-center sm:text-sm font-light text-gray-600'>Comparte</p>
+          <p className='text-sm text-center sm:text-sm font-light text-gray-600'>
+            Comparte esta información
+          </p>
           <div className='flex w-20 mx-auto justify-around'>
             <a
               href={`https://wa.me/?text=${sharingMessage}`}
