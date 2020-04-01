9be0e2e3ee4542bb7341667d13176a31af358587
Fernando López <ferlopezm94@gmail.com>
2020-03-31 23:36:44 -0600

    fix(source): add source initial text

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -132,6 +132,7 @@ ${today.tests && `- ${today.tests} personas estudiadas%0A`}
           </div>
         </div>
         <p className='text-sm text-center sm:text-sm font-light text-gray-600'>
+          Fuente:{' '}
           <a
             href='https://www.gob.mx/salud/documentos/coronavirus-covid-19-comunicado-tecnico-diario-238449'
             target='_blank'
