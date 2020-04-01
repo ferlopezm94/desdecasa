83412b6b87422d37f4da30e68831902b39ac8862
Fernando López <ferlopezm94@gmail.com>
2020-03-31 23:26:11 -0600

    fix(analytics): run only on production

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -19,9 +19,8 @@ const hour = moment().format('HH');
 const minute = moment().format('mm');
 
 export const initGA = () => {
-  console.log('init-ga start');
-
-  if (ReactGA.initialize) {
+  if (process.env.GATSBY_ENV && ReactGA.initialize) {
+    console.log('init-ga start');
     ReactGA.initialize('UA-162265508-1');
 
     if (window) {
@@ -32,7 +31,7 @@ export const initGA = () => {
 
 export const initAmplitude = () => {
   try {
-    if (amplitude.getInstance) {
+    if (process.env.GATSBY_ENV && amplitude.getInstance) {
       console.log('init-amplitude start');
       amplitude.getInstance().init('c886889426a03f94edcbb3980845d7bf', undefined, {
         sessionTimeout: 5 * 60 * 1000,
@@ -45,7 +44,7 @@ export const initAmplitude = () => {
 
 export const sendAmplitudeEvent = (eventName: Event) => {
   try {
-    if (amplitude.getInstance) {
+    if (process.env.GATSBY_ENV && amplitude.getInstance) {
       const defaultProperties = { year, month, day, hour, minute };
       console.log(eventName, defaultProperties);
       amplitude.getInstance().logEvent(eventName, defaultProperties);
