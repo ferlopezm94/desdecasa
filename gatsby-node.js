25121576bea4aa7c55a98369e0d508580d5979e2
Fernando López <ferlopezm94@gmail.com>
2020-03-31 23:32:49 -0600

    feat(states): add template page for every state

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,64 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+
+const statesNames = [
+  'Aguascalientes',
+  'Baja California',
+  'Baja California Sur',
+  'Campeche',
+  'Chiapas',
+  'Chihuahua',
+  'Ciudad de México',
+  'Coahuila',
+  'Colima',
+  'Durango',
+  'Guanajuato',
+  'Guerrero',
+  'Hidalgo',
+  'Jalisco',
+  'Michoacán',
+  'Morelos',
+  'Estado de México',
+  'Nayarit',
+  'Nuevo León',
+  'Oaxaca',
+  'Puebla',
+  'Querétaro',
+  'Quintana Roo',
+  'San Luis Potosí',
+  'Sinaloa',
+  'Sonora',
+  'Tabasco',
+  'Tamaulipas',
+  'Tlaxcala',
+  'Veracruz',
+  'Yucatán',
+  'Zacatecas',
+];
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions;
+  const template = path.resolve(`src/templates/index.tsx`);
+
+  statesNames.forEach(stateName => {
+    const path = stateName
+      .toLowerCase()
+      .replace(new RegExp(' ', 'g'), '-')
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+    console.log('state', stateName, path);
+
+    createPage({
+      path,
+      component: template,
+      context: {
+        stateName,
+      },
+    });
+  });
+};
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'build-html') {
