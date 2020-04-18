954e65259611078c8bc8978040d3f32a6db5baeb
Fernando López <ferlopezm94@gmail.com>
2020-04-18 18:15:50 -0500

    fix(state): remove specific state page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,58 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-const path = require('path');
-
-// Data
-const today = require('./src/data/2020-04-17.json');
-const yesterday = require('./src/data/2020-04-16.json');
-
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions;
-  const template = path.resolve('src/templates/index.tsx');
-
-  if (Object.keys(today).length !== Object.keys(yesterday).length) {
-    throw new Error(
-      `Length mismatch ${Object.keys(today).length} !== ${Object.keys(yesterday).length}`,
-    );
-  }
-
-  Object.keys(today).forEach(stateName => {
-    if (stateName === 'Total') {
-      return;
-    }
-
-    const path =
-      stateName === 'Total'
-        ? '/'
-        : stateName
-            .toLowerCase()
-            .replace(new RegExp(' ', 'g'), '-')
-            .normalize('NFD')
-            .replace(/[\u0300-\u036f]/g, '');
-    const todayData = today[stateName];
-    const yesterdayData = yesterday[stateName];
-
-    if (!todayData || !yesterdayData) {
-      throw new Error(`Data unavailable for ${stateName}`);
-    }
-
-    console.log('state', stateName, path);
-
-    createPage({
-      path,
-      component: template,
-      context: {
-        slug: path,
-        stateName: stateName === 'Total' ? 'México' : stateName,
-        today: todayData,
-        yesterday: yesterdayData,
-        date: '2020-04-17',
-      },
-    });
-  });
-};
-
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
