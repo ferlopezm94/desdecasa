93989cc765a36afba79fef395dcdf5b519609ad8
Fernando López <ferlopezm94@gmail.com>
2020-04-25 18:10:07 -0500

    feat(race): add sliding background

---
+++ src/pages/carrera/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Race = () => {
+  return (
+    <div className='w-screen h-screen overflow-hidden'>
+      <div className='h-full sliding-background'></div>
+    </div>
+  );
+};
+
+export default Race;
