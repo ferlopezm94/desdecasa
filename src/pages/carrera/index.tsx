17f0d9f369f4c4962a4a6846f929f1e2b7e4b038
Fernando López <ferlopezm94@gmail.com>
2020-04-25 18:40:51 -0500

    feat(race): create reusable player

--- src/pages/carrera/index.tsx
+++ src/pages/carrera/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const Race = () => {
   return (
     <div className='w-screen h-screen overflow-hidden'>
       <div className='h-full sliding-background'></div>
+      <Player
+        score={90}
+        bottom={30}
+        src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
+      />
+      <Player
+        score={70}
+        bottom={25}
+        src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
+      />
+      <Player
+        score={50}
+        bottom={20}
+        src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
+      />
     </div>
   );
 };
 
 export default Race;
+
+const run = (score: number) => keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(${score}vw);
+  }
+`;
+
+interface AnimationProps {
+  score: number;
+  bottom: number;
+}
+
+const Player = styled.img<AnimationProps>`
+  position: absolute;
+  height: 3rem;
+  bottom: ${props => props.bottom}%;
+  animation: ${props => run(props.score)} 10s forwards;
+`;
