c44ea6d80685838b9f8a0baedd84e14fca1d9033
Fernando López <ferlopezm94@gmail.com>
2020-04-25 19:20:44 -0500

    feat(race): display top ten players

--- src/pages/carrera/index.tsx
+++ src/pages/carrera/index.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
+import historicalTotal from './../../data/historicalTotal.json';
+
+interface StateData {
+  stateName: string;
+  confirmed: number;
+}
+
+const getTopTen = () => {
+  console.log('get-top-ten');
+  console.log(Object.entries(historicalTotal));
+  const topTen: StateData[] = [];
+
+  for (const [key, value] of Object.entries(historicalTotal)) {
+    if (key !== 'Dates' && key !== 'Total') {
+      topTen.push({
+        stateName: key,
+        // @ts-ignore
+        confirmed: value.confirmed[value.confirmed.length - 1],
+      });
+    }
+  }
+
+  topTen.sort((state1, state2) => (state1.confirmed < state2.confirmed ? 1 : -1));
+  console.log('top-ten sort', JSON.stringify(topTen.slice(0, 10)));
+  return topTen.slice(0, 10);
+};
+
+const topTen = getTopTen();
+
 const Race = () => {
   return (
     <div className='w-screen h-screen overflow-hidden'>
       <div className='h-full sliding-background'></div>
-      <Player
-        score={90}
-        bottom={30}
-        src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
-      />
-      <Player
-        score={70}
-        bottom={25}
-        src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
-      />
-      <Player
-        score={50}
-        bottom={20}
-        src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
-      />
+      {topTen.map((state, index) => (
+        <Player
+          score={90 * (state.confirmed / topTen[0].confirmed)}
+          bottom={30 - 3 * index}
+          src='https://mobilegamegraphics.com/pvpaterno/GIF/wolf_run.gif'
+          key={index}
+        />
+      ))}
     </div>
   );
 };
