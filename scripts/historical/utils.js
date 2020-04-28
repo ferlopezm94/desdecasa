3d87c98d736b1d316cf52fa58c350119aef42d8f
Fernando López <ferlopezm94@gmail.com>
2020-04-27 19:16:11 -0500

    chore(data): update 2020-04-27

--- scripts/historical/utils.js
+++ scripts/historical/utils.js
@@ -27,6 +27,7 @@ const dailyDates = [
   '2020-04-24',
   '2020-04-25',
   '2020-04-26',
+  '2020-04-27',
 ];
 
 const informationTotal = {
--- scripts/parse/index.js
+++ scripts/parse/index.js
@@ -14,9 +14,9 @@ const {
   parseResultado,
 } = require('./utils');
 const { informationByState } = require('./initialInformationByState');
-const INPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-04-26.csv`;
-const OUTPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-04-26_parsed.csv`;
-const DATE = '2020-04-26';
+const INPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-04-27.csv`;
+const OUTPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-04-27_parsed.csv`;
+const DATE = '2020-04-27';
 
 const parseDetailedData = () => {
   console.log('create-daily-data-by-state start');
