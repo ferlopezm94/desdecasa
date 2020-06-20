34ea6c409254cc448e0806fb515583f98c3a5c65
Fernando López <ferlopezm94@gmail.com>
2020-06-19 19:15:18 -0500

    chore(data): update 2020-06-19

--- scripts/historical/utils.js
+++ scripts/historical/utils.js
@@ -80,6 +80,7 @@ const dailyDates = [
   '2020-06-16',
   '2020-06-17',
   '2020-06-18',
+  '2020-06-19',
 ];
 
 const informationTotal = {
--- scripts/parse/index.js
+++ scripts/parse/index.js
@@ -16,10 +16,10 @@ const {
   parseResultado,
 } = require('./utils');
 const { informationByState } = require('./initialInformationByState');
-const DATE = '2020-06-18';
-const OUTPUT_ZIP = `${__dirname}/../../src/data/raw/2020-06-18.zip`;
-const INPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-06-18.csv`;
-const OUTPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-06-18_parsed.csv`;
+const DATE = '2020-06-19';
+const OUTPUT_ZIP = `${__dirname}/../../src/data/raw/2020-06-19.zip`;
+const INPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-06-19.csv`;
+const OUTPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-06-19_parsed.csv`;
 
 const retrieveZipRawData = async () => {
   console.log('retrieveZipRawData');
