75306387dbfd38508ac142127d2c8c2e8d5e07c1
Fernando López <ferlopezm94@gmail.com>
2020-07-16 20:17:43 -0500

    chore(data): update 2020-07-16

--- scripts/parse/index.js
+++ scripts/parse/index.js
@@ -16,10 +16,10 @@ const {
   parseResultado,
 } = require('./utils');
 const { informationByState } = require('./initialInformationByState');
-const DATE = '2020-07-15';
-const OUTPUT_ZIP = `${__dirname}/../../src/data/raw/2020-07-15.zip`;
-const INPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-07-15.csv`;
-const OUTPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-07-15_parsed.csv`;
+const DATE = '2020-07-16';
+const OUTPUT_ZIP = `${__dirname}/../../src/data/raw/2020-07-16.zip`;
+const INPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-07-16.csv`;
+const OUTPUT_FILE_NAME = `${__dirname}/../../src/data/raw/2020-07-16_parsed.csv`;
 
 const retrieveZipRawData = async () => {
   console.log('retrieveZipRawData');
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ import { Stat } from './../components/Stat';
 import { initGA, initAmplitude, sendAmplitudeEvent } from './../utils/analytics';
 import { numberWithCommas } from './../utils/utils';
 
-import todayData from './../data/2020-07-15.json';
-import yesterdayData from './../data/2020-07-14.json';
+import todayData from './../data/2020-07-16.json';
+import yesterdayData from './../data/2020-07-15.json';
 
-const date = '2020-07-15';
+const date = '2020-07-16';
 console.log('today', todayData);
 console.log('yesterday', yesterdayData);
 
