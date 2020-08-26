634714483a7e0539f093bcd30c91ac8c7afa2948
Fernando López <ferlopezm94@gmail.com>
2020-08-25 20:22:29 -0500

    chore(data): update 2020-08-25

--- scripts/historical/utils.js
+++ scripts/historical/utils.js
@@ -148,6 +148,7 @@ const dailyDates = [
   '2020-08-22',
   '2020-08-23',
   '2020-08-24',
+  '2020-08-25',
 ];
 
 const informationTotal = {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ import { Stat } from './../components/Stat';
 import { initGA, initAmplitude, sendAmplitudeEvent } from './../utils/analytics';
 import { numberWithCommas } from './../utils/utils';
 
-import todayData from './../data/2020-08-24.json';
-import yesterdayData from './../data/2020-08-23.json';
+import todayData from './../data/2020-08-25.json';
+import yesterdayData from './../data/2020-08-24.json';
 
-const date = '2020-08-24';
+const date = '2020-08-25';
 console.log('today', todayData);
 console.log('yesterday', yesterdayData);
 
