85bd0447b8ef8b461ed7c98a2e219b8c978eda5e
Fernando López <ferlopezm94@gmail.com>
2021-02-02 23:15:33 -0600

    chore(data): update 2021-02-02

--- scripts/historical/utils.js
+++ scripts/historical/utils.js
@@ -309,6 +309,7 @@ const dailyDates = [
   '2021-01-30',
   '2021-01-31',
   '2021-02-01',
+  '2021-02-02',
 ];
 
 const informationTotal = {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ import { Stat } from './../components/Stat';
 import { initGA, initAmplitude, sendAmplitudeEvent } from './../utils/analytics';
 import { numberWithCommas } from './../utils/utils';
 
-import todayData from './../data/2021-02-01.json';
-import yesterdayData from './../data/2021-01-31.json';
+import todayData from './../data/2021-02-02.json';
+import yesterdayData from './../data/2021-02-01.json';
 
-const date = '2021-02-01';
+const date = '2021-02-02';
 console.log('today', todayData);
 console.log('yesterday', yesterdayData);
 
